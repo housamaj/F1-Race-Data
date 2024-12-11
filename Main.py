1fd13709226330b5107f11e784dfc28bdcc88760
housamaj <housamaj@dukes.jmu.edu>
2024-12-11 15:47:10 -0500

    12/11/24

--- Main.py
+++ Main.py
@@ -4,19 +4,15 @@ import pandas as pd
 from matplotlib.pyplot import xlabel, ylabel, title
 from scipy import stats
 
-# Load the pit stops data
+"""
+Question 1: What was the overall change in average pit stop time?
+"""
+
 pit_data = pd.read_csv('pit_stops.csv')
-
-# Extract raceId and milliseconds columns
 pit_race_id = pit_data[['raceId','milliseconds']]
-
-# Convert to numpy array
 pit_race_id_numpy = pit_race_id.to_numpy()
 
-# Initialize lists
 average_list = []
-
-# Initialize variables
 current_race_id = pit_race_id_numpy[0, 0]
 pit_list = []
 list_of_races = []
@@ -41,38 +37,35 @@ for i in range(len(pit_race_id_numpy)):
 if pit_list:
     average_list.append(np.mean(pit_list))
 
-"""
-Question 1: What was the overall change in average pit stop time?
-"""
-average_list_seconds = np.divide(average_list,1000)
+average_list_seconds = np.divide(average_list,1000) # Convert to seconds
 coefficients = np.polyfit(list_of_races, average_list_seconds, 1)  # 1 for a linear trendline
 p = np.poly1d(coefficients)
-plt.plot(list_of_races,p(average_list_seconds),"r--")
-plt.bar(list_of_races,average_list_seconds)
+plt.plot(list_of_races,p(average_list_seconds),"r--") # Plot trendline
+plt.bar(list_of_races,average_list_seconds) # Plot graph
 xlabel('Race ID')
 ylabel('Time (s)')
 title('Change in average pit stop times from 2011 to 2024')
 plt.show()
 
 m, b = coefficients
-print(f"Trendline equation: y = {m:.2f}x + {b:.2f}")
+print(f"Question 1: Trendline equation: y = {m:.2f}x + {b:.2f}") # Print out trendline equation
 
 """
 Question 2: What is the fastest pit stop time overall?
 """
 
-fastest_race_time = np.min(pit_race_id_numpy[:,1]) / 1000
+fastest_race_time = np.min(pit_race_id_numpy[:,1]) / 1000 # Convert to seconds
 
-print("The fastest pit stop time from 2011 to 2024 was " + str(fastest_race_time) + " seconds")
+print("Question 2: The fastest pit stop time from 2011 to 2024 was " + str(fastest_race_time) + " seconds")
 
 """
 Question 3: Which race ID had the fastest pit stop time?
 """
 
-fastest_race_index = np.argmin(pit_race_id_numpy[:,1])
+fastest_race_index = np.argmin(pit_race_id_numpy[:,1]) # Return index of the fastest race
 fastest_race_id = pit_race_id_numpy[fastest_race_index,0]
 
-print("The race ID with the fastest pit stop is " + str(fastest_race_id))
+print("Question 3: The race ID with the fastest pit stop is " + str(fastest_race_id))
 
 """
 Question 4: What is the fastest race lap time recorded for a selected circuit?
@@ -84,14 +77,16 @@ lap_time_list = []
 
 for i in range(len(lap_time_array)):
     raceId, driverId, milli = lap_time_array[i]
-    if raceId == 1125:
+    if raceId == 1125: # race ID for the Chinese Grand Prix
         lap_time_list.append(milli)
 
-fastest_lap_time = np.min(lap_time_list) / 1000
-print("The fastest lap time for the Chinese Grand Prix is " + str(fastest_lap_time) + " seconds")
+fastest_lap_time = np.min(lap_time_list) / 1000 # Convert to seconds
+print("Question 4: The fastest lap time for the Chinese Grand Prix is " + str(fastest_lap_time) + " seconds")
+
+# china_dates_chrono = ['2004-09-26','2005-10-16','2006-10-01','2007-10-07','2008-10-19','2009-04-19','2010-04-18','2011-04-17','2012-04-15','2012-04-15','2013-04-14','2014-04-20','2015-04-12','2016-04-17','2017-04-09''2018-04-15','2019-04-14','2024-04-21']
 
 """
-Question 5: How has lap time decreased over time for a selected circuit?
+Question 5: How has average lap time changed over time for a selected circuit?
 """
 
 races_data = pd.read_csv('races.csv')
@@ -106,10 +101,6 @@ for i in range(len(races_array)):
     if name == "Chinese Grand Prix":
         china_race_ids.append(raceId)
         china_dates.append(date)
-
-print(china_dates)
-
-china_dates_chrono = ['2004-09-26','2005-10-16','2006-10-01','2007-10-07','2008-10-19','2009-04-19','2010-04-18','2011-04-17','2012-04-15','2012-04-15','2013-04-14','2014-04-20','2015-04-12','2016-04-17','2017-04-09''2018-04-15','2019-04-14','2024-04-21']
 
 china_lap_list = []
 average_CGP_time_list = []
@@ -139,7 +130,7 @@ for i in range(len(lap_time_array)):
 if china_lap_list:
     average_CGP_time_list.append(np.mean(china_lap_list))
 
-average_CGP_seconds = np.divide(average_CGP_time_list,1000)
+average_CGP_seconds = np.divide(average_CGP_time_list,1000) # Convert to seconds
 
 plt.bar(china_dates,average_CGP_seconds)
 plt.xlabel("Date", fontsize=10 )
@@ -160,14 +151,17 @@ driver_winner_list = []
 for i in range(len(driver_array)):
     driverId, position = driver_array[i]
 
+    # Loop through array and count append the driver ID each time they finish in position 1
     if position == 1:
         driver_winner_list.append(driverId)
     else:
         continue
 
-driver_winner = stats.mode(driver_winner_list)
+driver_winner = stats.mode(driver_winner_list) # Return the mode and number of occurrences using SciPy
 print(driver_winner)
-print("The driver with the most wins ever is Driver ID 1 with 125 wins. This is Lewis Hamilton")
+
+# Evaluate the print statement visually
+print("Question 6: The driver with the most wins ever is Driver ID 1 with 125 wins. This is Lewis Hamilton")
 
 """
 Question 7: What team has the most wins?
@@ -179,15 +173,18 @@ constructor_array = constructor_data[['constructorId','position']].to_numpy()
 constructor_win_list = []
 
 for i in range(len(constructor_array)):
-
     constructorId, position = constructor_array[i]
+
+    # Loop through array and count append the constructor ID each time they finish in position 1
     if position == 1:
         constructor_win_list.append(constructorId)
     else:
         continue
 
-constructor_winner = stats.mode(constructor_win_list)
+constructor_winner = stats.mode(constructor_win_list) # Return the mode and number of occurrences using SciPy
 print(constructor_winner)
+
+# Evaluate the print statement visually
 print("The constructor with the most wins is Constructor ID 6 with 234 wins. This is Ferrari")
 
 
